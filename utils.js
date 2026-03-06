332c834d5cff9151243a1c1abdca5083792bdf19
Ponti2014 <94399236+Ponti2014@users.noreply.github.com>
2026-03-06 09:47:43 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T06:46:58.475Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T06:47:45.807Z
+Commit: Fix broken links in documentation

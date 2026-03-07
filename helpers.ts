4c8708c978a6aa1db7e57c7bdbdc97ca748dac26
Ponti2014 <94399236+Ponti2014@users.noreply.github.com>
2026-03-07 10:49:42 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T07:49:08.357Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-07T07:49:43.062Z
+Commit: Fix memory leak in event handlers

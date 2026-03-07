1db3a9216240400dc32404292b0aa5bd02060843
Ponti2014 <94399236+Ponti2014@users.noreply.github.com>
2026-03-07 10:49:08 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T07:49:06.861Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-07T07:49:08.357Z
+Commit: Resolve null pointer exception in user service

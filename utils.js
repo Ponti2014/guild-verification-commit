9a4da568925111efaa7c8c5f6ff5800f6e82b6f1
Ponti2014 <94399236+Ponti2014@users.noreply.github.com>
2026-03-07 10:49:47 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T07:49:29.106Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-07T07:49:48.286Z
+Commit: Fix memory leak in event handlers

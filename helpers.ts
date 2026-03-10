9f9785ae01275c94a4da386ba7d848f0856fbc9a
Ponti2014 <94399236+Ponti2014@users.noreply.github.com>
2026-03-10 09:37:22 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T06:37:14.570Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-10T06:37:22.314Z
+Commit: Patch API endpoint error handling

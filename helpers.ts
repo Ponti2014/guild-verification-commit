1834184698ac3760b494692f6a335f65597817c0
Ponti2014 <94399236+Ponti2014@users.noreply.github.com>
2026-03-10 09:37:47 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T06:37:37.614Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-10T06:37:47.399Z
+Commit: Add multi-language support

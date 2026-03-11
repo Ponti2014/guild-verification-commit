94b175f07d6108bbdd4755c93c68b77f3117b029
Ponti2014 <94399236+Ponti2014@users.noreply.github.com>
2026-03-11 09:51:59 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T14:48:54.549Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-11T06:52:00.505Z
+Commit: Resolve null pointer exception in user service

5865a086a6122aa7fa480fe6bba4654a68a14be8
Ponti2014 <94399236+Ponti2014@users.noreply.github.com>
2026-03-11 09:53:16 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T06:52:46.908Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-11T06:53:17.477Z
+Commit: Fix memory leak in event handlers

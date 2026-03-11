b1beb104f8bc092b889d84ac19e977dfc9d3f603
Ponti2014 <94399236+Ponti2014@users.noreply.github.com>
2026-03-11 09:53:28 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T06:53:10.929Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-11T06:53:29.130Z
+Commit: Fix CSS styling bug on mobile devices

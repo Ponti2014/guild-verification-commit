6eb19d32a3aefcadbbadc4be5d897411cad979ad
Ponti2014 <94399236+Ponti2014@users.noreply.github.com>
2026-03-11 09:52:49 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T06:52:13.973Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-11T06:52:50.909Z
+Commit: Patch API endpoint error handling

de4400e0ddeb41151b1a9ddaf996015b62c27f86
Ponti2014 <94399236+Ponti2014@users.noreply.github.com>
2026-03-11 09:52:34 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T06:52:31.580Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-11T06:52:35.889Z
+Commit: Patch API endpoint error handling
